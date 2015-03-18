eb31546ff0717335e67f1887afa8e0a593d30039
Martin Stroet <martin.stroet@gmail.com>
2015-03-18 23:06:05 +1000

    numpyified version with dynamic test cases

--- rmsd.py
+++ rmsd.py
@@ -1,40 +1,33 @@
-import math
-import numpy
+from numpy import sqrt, mean, square, min
+import numpy as np
+from scipy.spatial.distance import cdist
 
 def alignPointsOnPoints(point_list1, point_list2):
-    cog1 = center_of_mass(point_list1)
-    cog2 = center_of_mass(point_list2)
-    translate_cog=substract(cog2, cog1)
-    print translate_cog
-    print "Aligning by translating from center of mass difference"
-    return map(lambda point:add(point, translate_cog), point_list1)
+    point_array1 = np.array(point_list1)
+    point_array2 = np.array(point_list2)
+    cog1 = center_of_geometry(point_array1)
+    cog2 = center_of_geometry(point_array2)
+    translate_cog = cog2 - cog1
+    return point_array1 + translate_cog
 
 def rmsd(point_list1, point_list2):
-    rmsd = 0.
-    n_points = len(point_list1)
-    for point1 in point_list1:
-        #print "Point1: {0}".format(point1)
-        closest_distance = min([ distance(point1, point2) for point2 in point_list2])
-        #print "Closest_distance: {0}".format(closest_distance)
-        rmsd += closest_distance
-    rmsd = math.sqrt( rmsd / n_points )
-    print "RMSD: {0}".format(rmsd)
+    point_array1 = np.array(point_list1)
+    point_array2 = np.array(point_list2)
+    distance_matrix = get_distance_matrix(point_array1, point_array2)
+    
+    # Do you like my lisp skills?
+    # This convoluted one-liner computes the square (R)oot of the (M)ean (S)quared (M)inimum (D)istances
+    # We should call it the RMSMD :).
+    # I think this is my favourite one-liner ever! (Probably because it look me 1 hour to construct and it's still beautiful)
+    rmsd = sqrt( mean( square( min( distance_matrix, axis=0 ) ) ) )
     return rmsd
 
 def distance(point1, point2):
-    return math.sqrt( (point1[0]-point2[0])**2 + (point1[1]-point2[1])**2 + (point1[2]-point2[2])**2 )
+    return np.linalg.norm(point1 - point2)
 
-def center_of_mass(point_list):
-    x = sum([ point[0] for point in point_list])
-    y = sum([ point[1] for point in point_list])
-    z = sum([ point[2] for point in point_list])
-    return map(lambda x:x/len(point_list), [x, y, z])
+def center_of_geometry(point_array):
+    return np.mean(point_array, axis=0)
 
-def substract(point1, point2):
-    return [ point1[0]-point2[0], point1[1]-point2[1], point1[2]-point2[2]  ]
-
-def add(point1, point2):
-    return [ point1[0]+point2[0], point1[1]+point2[1], point1[2]+point2[2]  ]
-
-if __name__ == "__main__":
-    main(point_list1, point_list2)
+def get_distance_matrix(x, y):
+    return cdist(x, y, metric='euclidean')
+    
--- test_rmsd.py
+++ test_rmsd.py
@@ -1,28 +1,37 @@
 import unittest
+import sys
 import rmsd
+import logging
+logging.basicConfig(level=logging.DEBUG, format='    [%(levelname)s] - %(message)s')
 
 class Test_RMSD(unittest.TestCase):
-    def test_simple(self):
-        set1 = [ [0.,0.,0.], [1.,1.,1.] ]
-        set2 = [ [1.,1.,1.], [0.,0.,0.] ]
-        rmsd.rmsd(set1, set2)
-        set1_aligned = rmsd.alignPointsOnPoints(set1, set2)
-        self.assertAlmostEqual( rmsd.rmsd(set2, set1_aligned), 0.)
+    pass
 
-    def test_harder(self):
-        set1 = [ [0.,0.,0.], [1.,1.,1.] ]
-        set2 = [ [1.,1.,1.], [2.,2.,2.] ]
-        rmsd.rmsd(set1, set2)
-        set1_aligned = rmsd.alignPointsOnPoints(set1, set2)
-        self.assertAlmostEqual( rmsd.rmsd(set2, set1_aligned), 0.)
+def test_alignment_generator(points1, points2):
+    def test(self):
+        sys.stderr.write("\n")
+        logging.info("RMSD before alignment: {0:.4f}".format(rmsd.rmsd(points1, points2)))
+        points1_aligned = rmsd.alignPointsOnPoints(points1, points2)
+        logging.info("RMSD after alignment: {0:.4f}".format(rmsd.rmsd(points1_aligned, points2)))
+        self.assertAlmostEqual( rmsd.rmsd(points1_aligned, points2), 0.)
+    return test
 
-    def test_even_harder(self):
-        set1 = [ [0.,0.,0.], [1.,1.,1.], [-1.,-1.,-1.] ]
-        set2 = [ [2.,2.,2.], [3.,3.,3.], [4.,4.,4.] ]
-        rmsd.rmsd(set1, set2)
-        set1_aligned = rmsd.alignPointsOnPoints(set1, set2)
-        self.assertAlmostEqual( rmsd.rmsd(set2, set1_aligned), 0.)
 
 if __name__ == "__main__":
+    batch_tests = (("simple", 
+                    [ [0.,0.,0.], [1.,1.,1.] ], 
+                    [ [1.,1.,1.], [0.,0.,0.] ]),
+                   ("harder", 
+                    [ [0.,0.,0.], [1.,1.,1.] ], 
+                    [ [1.,1.,1.], [2.,2.,2.] ]),
+                   ("even_harder", 
+                    [ [0.,0.,0.], [1.,1.,1.], [-1.,-1.,-1.] ], 
+                    [ [2.,2.,2.], [3.,3.,3.], [4.,4.,4.] ]),
+                   )
+    for name, points1, points2 in batch_tests:
+        # Create the tests dynamically
+        # Source : http://stackoverflow.com/questions/32899/how-to-generate-dynamic-parametrized-unit-tests-in-python
+        test = test_alignment_generator(points1, points2)
+        setattr(Test_RMSD, "test_" + name, test)
     suite = unittest.TestLoader().loadTestsFromTestCase(Test_RMSD)
     unittest.TextTestRunner(verbosity=4).run(suite)

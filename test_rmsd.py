98f2f9c6fd2b3a6e3a8aa994e2c7c46ac84003db
Bertrand Caron <bertrand.fx.caron@gmail.com>
2015-03-19 08:41:55 +1000

    Add expected RMSD and a simple (failing) rotation test

--- test_rmsd.py
+++ test_rmsd.py
@@ -7,31 +7,39 @@ logging.basicConfig(level=logging.DEBUG, format='    [%(levelname)s] - %(message
 class Test_RMSD(unittest.TestCase):
     pass
 
-def test_alignment_generator(points1, points2):
+def test_alignment_generator(points1, points2, expected_rmsd):
     def test(self):
         sys.stderr.write("\n")
         logging.info("RMSD before alignment: {0:.4f}".format(rmsd.rmsd(points1, points2)))
         points1_aligned = rmsd.alignPointsOnPoints(points1, points2)
         logging.info("RMSD after alignment: {0:.4f}".format(rmsd.rmsd(points1_aligned, points2)))
-        self.assertAlmostEqual( rmsd.rmsd(points1_aligned, points2), 0.)
+        logging.info("Expected RMSD: {0:.4f}".format(expected_rmsd))
+        self.assertAlmostEqual( rmsd.rmsd(points1_aligned, points2), expected_rmsd, places=3)
     return test
 
 
 if __name__ == "__main__":
-    batch_tests = (("simple", 
+    batch_tests = (("translation_simple", 
                     [ [0.,0.,0.], [1.,1.,1.] ], 
-                    [ [1.,1.,1.], [0.,0.,0.] ]),
-                   ("harder", 
+                    [ [1.,1.,1.], [0.,0.,0.] ],
+                    0.),
+                   ("translation_harder", 
                     [ [0.,0.,0.], [1.,1.,1.] ], 
-                    [ [1.,1.,1.], [2.,2.,2.] ]),
-                   ("even_harder", 
+                    [ [1.,1.,1.], [2.,2.,2.] ],
+                    0.),
+                   ("translation_even_harder", 
                     [ [0.,0.,0.], [1.,1.,1.], [-1.,-1.,-1.] ], 
-                    [ [2.,2.,2.], [3.,3.,3.], [4.,4.,4.] ]),
+                    [ [2.,2.,2.], [3.,3.,3.], [4.,4.,4.] ],
+                    0.),
+                   ("rotation_90_degrees", 
+                    [ [1.,0.,0.], [0.,0.,0.] ], 
+                    [ [0.,1.,0.], [0.,0.,0.] ],
+                    0.),
                    )
-    for name, points1, points2 in batch_tests:
+    for name, points1, points2, expected_rmsd in batch_tests:
         # Create the tests dynamically
         # Source : http://stackoverflow.com/questions/32899/how-to-generate-dynamic-parametrized-unit-tests-in-python
-        test = test_alignment_generator(points1, points2)
+        test = test_alignment_generator(points1, points2, expected_rmsd)
         setattr(Test_RMSD, "test_" + name, test)
     suite = unittest.TestLoader().loadTestsFromTestCase(Test_RMSD)
     unittest.TextTestRunner(verbosity=4).run(suite)

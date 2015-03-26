c111a810fd315900925a2d87f7595a39edcec96a
Bertrand Caron <bertrand.fx.caron@gmail.com>
2015-03-26 17:09:36 +1000

    Add first passing example with rotationnal component.
    The rotation_sp2_staggered is still failing thought, and has a
    translationnal component whose source I can't find ...
    
    Also, we need to make sure that point1 is not on the center of geometry,
    otherwise all the rotmat end up being id(3) and nothing moves ...

--- rmsd.py
+++ rmsd.py
@@ -1,15 +1,66 @@
-from numpy import sqrt, mean, square, min
+from numpy import sqrt, mean, square, min, minimum
 import numpy as np
 from scipy.spatial.distance import cdist
+from Vector import Vector, rotmat
+import math
+
+RMSD_TOLERANCE = 1E-3
 
 def alignPointsOnPoints(point_list1, point_list2):
     point_array1 = np.array(point_list1)
     point_array2 = np.array(point_list2)
     cog1 = center_of_geometry(point_array1)
     cog2 = center_of_geometry(point_array2)
+
+    # First, remove tranlational part
     translate_cog = cog2 - cog1
-    aligned_point_array = point_array1 + translate_cog
-    return aligned_point_array
+    print "Translational componenet: {0}".format(translate_cog)
+    aligned_point_array1 = point_array1 + translate_cog
+
+    # Break now if there are no rotationnal component
+    if rmsd(aligned_point_array1, point_array2) <= RMSD_TOLERANCE: return aligned_point_array1
+
+    # Them try all the rotation that put one on the atom of the first set into one of the atoms of the second sets
+    # There are N such rotations
+    point1_vector = Vector(point_list1[0])
+    minimum_rmsd = 100.
+    best_aligned_point_array1 = aligned_point_array1
+    #print "Vector 1 is: {0}".format(point1_vector)
+
+    for point2 in point_list2:
+
+        point2_vector = Vector(point2)
+        #print "\nVector 2 is: {0}".format(point2_vector)
+
+        # If the points are already superimposed, continue as the rotation matrix would be [[Nan, Nan, Nan], ...
+        if point1_vector == point2_vector:
+            print "{0} and {1} are superimposed".format(point1_vector, point2_vector)
+            continue
+
+        r = rotmat(point2_vector, point1_vector)
+        print "\nMatrix rotating {0} onto {1}:".format(point1_vector, point2_vector)
+        print r
+        rotated_point1_vector = point1_vector.left_multiply(r)
+        rotated_point_array1 = np.dot(point_array1, r)
+        print "Coordinate before rotation:"
+        print aligned_point_array1
+        print "Coordinate after rotation:"
+        print rotated_point_array1
+
+        #print rotated_point_array1
+        #print point_array2
+
+        current_rmsd = rmsd_array(rotated_point_array1, point_array2)
+        minimum_rmsd = minimum(minimum_rmsd, current_rmsd)
+        if current_rmsd == minimum_rmsd: best_aligned_point_array1 = rotated_point_array1
+
+        print "    New RMSD after rotation: {0}".format(current_rmsd)
+        #print "{0} has been rotated to {1}".format(point1_vector, rotated_point_array1)
+
+        if current_rmsd <= RMSD_TOLERANCE:
+            break
+
+    return best_aligned_point_array1
 
 def rmsd(point_list1, point_list2):
     point_array1 = np.array(point_list1)

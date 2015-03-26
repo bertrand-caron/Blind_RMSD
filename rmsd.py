14ba449fc02f34a203dbe2136f8a2bb2a98f4d9b
Bertrand Caron <bertrand.fx.caron@gmail.com>
2015-03-26 14:49:14 +1000

    Add rmsd and rmsd_array

--- rmsd.py
+++ rmsd.py
@@ -8,11 +8,15 @@ def alignPointsOnPoints(point_list1, point_list2):
     cog1 = center_of_geometry(point_array1)
     cog2 = center_of_geometry(point_array2)
     translate_cog = cog2 - cog1
-    return point_array1 + translate_cog
+    aligned_point_array = point_array1 + translate_cog
+    return aligned_point_array
 
 def rmsd(point_list1, point_list2):
     point_array1 = np.array(point_list1)
     point_array2 = np.array(point_list2)
+    return rmsd_array(point_array1, point_array2)
+
+def rmsd_array(point_array1, point_array2):
     distance_matrix = get_distance_matrix(point_array1, point_array2)
     
     # Do you like my lisp skills?

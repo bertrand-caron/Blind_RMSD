5824e7e68e6c56ff97a1dd960c55f883d02ea9a1
Bertrand Caron <bertrand.fx.caron@gmail.com>
2015-04-15 14:55:18 +1000

    Start working towards the automation of the downloading of the tests

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-test: Vector.py charnley_rmsd/kabsch.py testing/benzene1.pdb testing/benzene2.pdb testing/ethanol1.pdb testing/ethanol2.pdb testing/methanol1.pdb testing/methanol2.pdb pmx testing/sulfuric_acid1.pdb testing/sulfuric_acid2.pdb
+test: Vector.py charnley_rmsd/kabsch.py pmx
 	python test_rmsd.py
 
 test-pymol:
@@ -21,31 +21,6 @@ charnley_rmsd:
 
 testing:
 	mkdir $@
-
-testing/benzene1.pdb: testing
-	wget "http://compbio.biosci.uq.edu.au/atb/download.py?molid=21364&outputType=top&dbfile=pdb_fromuser" -O $@
-
-# This molecule is actually restricted access only ...
-#testing/benzene2.pdb: testing
-#	wget "http://compbio.biosci.uq.edu.au/atb/download.py?molid=8479&outputType=top&dbfile=pdb_fromuser" -O $@
-
-testing/ethanol1.pdb: testing
-	wget "http://compbio.biosci.uq.edu.au/atb/download.py?molid=22354&outputType=top&dbfile=pdb_fromuser" -O $@
-
-testing/ethanol2.pdb: testing
-	wget "http://compbio.biosci.uq.edu.au/atb/download.py?molid=23009&outputType=top&dbfile=pdb_fromuser" -O $@
-
-testing/sulfuric_acid1.pdb: testing
-	wget "http://compbio.biosci.uq.edu.au/atb/download.py?molid=1745&outputType=top&dbfile=pdb_fromuser" -O $@
-
-testing/sulfuric_acid2.pdb: testing
-	wget "http://compbio.biosci.uq.edu.au/atb/download.py?molid=20681&outputType=top&dbfile=pdb_fromuser" -O $@
-
-#testing/methanol1.pdb: testing
-#	wget "http://compbio.biosci.uq.edu.au/atb/download.py?molid=15607&outputType=top&dbfile=pdb_fromuser" -O $@
-
-#testing/methanol2.pdb: testing
-#	wget "http://compbio.biosci.uq.edu.au/atb/download.py?molid=19901&outputType=top&dbfile=pdb_fromuser" -O $@
 
 pmx:
 		echo -e "Please install pmx\n\n"
--- test_rmsd.py
+++ test_rmsd.py
@@ -1,5 +1,6 @@
 import unittest
 import sys
+import os
 import rmsd
 import logging
 import math
@@ -10,6 +11,9 @@ import pmx
 from copy import deepcopy
 logging.basicConfig(level=logging.DEBUG, format='    [%(levelname)s] - %(message)s')
 import yaml
+sys.path.append("../")
+import urllib2
+from os.path import exists, dirname, isdir
 
 numerical_tolerance = 1e-5
 
@@ -23,9 +27,28 @@ def test_alignment_generator(points1, points2, expected_rmsd):
         self.assertLessEqual( rmsd.rmsd(points1_aligned, points2), expected_rmsd)
     return test
 
-def molecule_test_alignment_generator(molecule_name, expected_rmsd):
-    def test(self):
+FILE_TEMPLATE = "testing/{molecule_name}/{molecule_name}{version}.{extension}"
 
+DOWNLOAD_TEMPLATES = { 'pdb': 'http://compbio.biosci.uq.edu.au/atb/download.py?molid={molid}&outputType=top&dbfile=pdb_fromuser',
+                       'yml': 'http://compbio.biosci.uq.edu.au/atb-new/api/molecules/mol_data.py?molid={molid}',
+                     }
+
+def download_molecule_files(molecule_name, molids):
+    for version in [1,2]:
+        molid = molids[version - 1]
+        for extension in ['pdb', 'yml']:
+            file_name = FILE_TEMPLATE.format(molecule_name=molecule_name, extension=extension, version=version)
+            if not exists( dirname(file_name)): os.mkdir( dirname(file_name) )
+            if not exists(file_name):
+                with open(file_name, 'w') as fh:
+                    response = urllib2.urlopen(DOWNLOAD_TEMPLATES[extension])
+                    fh.write( response.read() )
+
+def molecule_test_alignment_generator(test_datum, expected_rmsd):
+    def test(self):
+        molecule_name = test_datum['molecule_name']
+
+        download_molecule_files(molecule_name, [test_datum['id1'], test_datum['id2']])
         point_list1 = []
         point_list2 = []
         m1 = pmx.Model('testing/{molecule_name}1.pdb'.format(molecule_name=molecule_name))
@@ -61,8 +84,11 @@ class Test_RMSD(unittest.TestCase):
             self.fail('%s not less than or equal to %s within %f' % (repr(a), repr(b), numerical_tolerance))
 
 if __name__ == "__main__":
-    for molecule_name in TEST_MOLECULES:
-        test = molecule_test_alignment_generator(molecule_name, 0.15)
-        setattr(Test_RMSD, "test_" + molecule_name, test)
+    with open('test_data.yml') as fh:
+        test_data = yaml.load(fh.read())
+        print "Test data is:\n{0}\n".format(yaml.dump(test_data))
+        for test_datum in test_data:
+            test = molecule_test_alignment_generator(test_datum, 0.15)
+            setattr(Test_RMSD, "test_" + test_datum['molecule_name'], test)
     suite = unittest.TestLoader().loadTestsFromTestCase(Test_RMSD)
     unittest.TextTestRunner(verbosity=4).run(suite)

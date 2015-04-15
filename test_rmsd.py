23c1034fa5af6338d91c5406fb6d80b88b2721b0
Bertrand Caron <bertrand.fx.caron@gmail.com>
2015-04-15 14:54:54 +1000

    Remove batch tests

--- test_rmsd.py
+++ test_rmsd.py
@@ -60,69 +60,7 @@ class Test_RMSD(unittest.TestCase):
         if not a <= b + numerical_tolerance:
             self.fail('%s not less than or equal to %s within %f' % (repr(a), repr(b), numerical_tolerance))
 
-batch_tests = (
-#               ("translation_simple",
-#                [ [0.,0.,0.], [1.,1.,1.] ],
-#                [ [1.,1.,1.], [0.,0.,0.] ],
-#                None,
-#                0.),
-#               ("translation_harder",
-#                [ [0.,0.,0.], [1.,1.,1.] ],
-#                [ [1.,1.,1.], [2.,2.,2.] ],
-#                None,
-#                0.),
-#               ("translation_even_harder",
-#                [ [0.,0.,0.], [1.,1.,1.], [-1.,-1.,-1.] ],
-#                [ [2.,2.,2.], [3.,3.,3.], [4.,4.,4.] ],
-#                None,
-#                0.),
-#               ("rotation_90_degrees",
-#                [ [1.,0.,0.], [0.,0.,0.] ],
-#                [ [0.,1.,0.], [0.,0.,0.] ],
-#                None,
-#                0.),
-#               ("rotation_90_degrees_norm2",
-#                [ [2.,0.,0.], [0.,0.,0.] ],
-#                [ [0.,2.,0.], [0.,0.,0.] ],
-#                None,
-#                0.),
-#               ("rotation_90_degrees_norm2_T_shaped_180deg",
-#                [ [2.,0.,0.], [0.,0.,0.], [0.,2.,0.] ],
-#                [ [-2,0.,0.], [ 0.,-2.,0.], [0.,0.,0.] ],
-#                None,
-#                0.),
-#               ("rotation_90_degrees_norm2_T_shaped",
-#                [ [2.,0.,0.], [0.,0.,0.], [0.,2.,0.] ],
-#                [ [-2,0.,0.], [0.,2.,0.], [0.,0.,0.]],
-#                None,
-#                0.),
-#               ("rotation_90_degrees_norm2_T_shaped_asymmetric",
-#                [ [-2.,0.,0.], [2.,0.,0.], [0.,  2.,0.] ],
-#                [ [-2.,0.,0.], [2.,0.,0.], [0., -2.,0.] ],
-#                None,
-#                0.),
-#              ("rotation_sp2_staggered",
-#               [ [1.,1.,0.], [0.,-math.sqrt(2.),0.], [-1.,1.,0.] ],
-#               [ [0.,math.sqrt(2),0.], [-1.,-1.,0.], [1.,-1.,0.] ],
-#               180,
-#               0.),
-#               ("rotation_staggered_cubes",
-#                [ [1.,1.,0.], [1.,-1.,0.], [-1.,1.,0.], [-1.,-1.,0.] ],
-#                [ [0.,math.sqrt(2),0.], [0.,-math.sqrt(2),0.], [math.sqrt(2),0.,0.], [-math.sqrt(2),0.,0.] ],
-#                0.),
-#               ("rotation_staggered_cubes_3D",
-#                [ [1.,1.,1.], [1.,-1.,1.], [-1.,1.,1.], [-1.,-1.,1.], [1.,1.,-1.], [1.,-1.,-1.], [-1.,1.,-1.], [-1.,-1.,-1.]],
-#                [ [0.,math.sqrt(2),1.], [0.,-math.sqrt(2),1.], [math.sqrt(2),0.,1.], [-math.sqrt(2),0.,1.], [0.,math.sqrt(2),-1.], [0.,-math.sqrt(2),-1.], [math.sqrt(2),0.,-1.], [-math.sqrt(2),0.,-1.] ],
-#                0.,
-#                0.),
-               )
-
 if __name__ == "__main__":
-    for name, points1, points2, theoretical_solution, expected_rmsd in batch_tests:
-        # Create the tests dynamically
-        # Source : http://stackoverflow.com/questions/32899/how-to-generate-dynamic-parametrized-unit-tests-in-python
-        test = test_alignment_generator(points1, points2, expected_rmsd)
-        setattr(Test_RMSD, "test_" + name, test)
     for molecule_name in TEST_MOLECULES:
         test = molecule_test_alignment_generator(molecule_name, 0.15)
         setattr(Test_RMSD, "test_" + molecule_name, test)

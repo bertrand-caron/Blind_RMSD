3c38c0d2904f21c2092f3fa4ec28e73df6f2bdab
Bertrand Caron <bertrand.fx.caron@gmail.com>
2015-06-29 16:02:01 +1000

    Add first and second neighbours as the flavours, seem to do a lot of good

--- test_rmsd.py
+++ test_rmsd.py
@@ -155,13 +155,17 @@ def get_distance_matrix(test_datum, silent=True, debug=False, no_delete=False, m
     def first_order_neighbour_elements(data):
         return [ reduce(lambda x,y:x+y, sorted(map(lambda con: data['atoms'][con]['type'], conn_list)), '') for conn_list in [ atom['conn'] for index, atom in data['atoms'].items() ] ]
 
+    def second_order_neighbour_elements(data):
+        return [ reduce(lambda x,y:x+y, sorted([ data['atoms'][second_neighbour_index]['type'] for second_neighbour_index in reduce(lambda x,y: x+y, [ data['atoms'][conn]['conn'] for conn in conn_list ])])) for conn_list in [ atom['conn'] for index, atom in data['atoms'].items() ] ]
+
     def equivalence_list(data):
         return split_equivalence_group([ atom['equivalenceGroup'] for index, atom in data['atoms'].items() ])
 
     def flavour_list(data):
         eq_list = equivalence_list(data)
+        first_neighbour_list, second_neighbour_list = first_order_neighbour_elements(data), second_order_neighbour_elements(data)
         grouped_eq_list = group_by(equivalence_list(data), lambda x:x)
-        return [ len(grouped_eq_list[eq]) for eq in eq_list]
+        return [ str(len(grouped_eq_list[eq])) + '|' + first_neighbours + '|' + second_neighbours for eq, first_neighbours, second_neighbours in zip(eq_list, first_neighbour_list, second_neighbour_list) ]
 
     def element_list(data):
         return [ atom['type'] for index, atom in data['atoms'].items() ]

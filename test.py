142f2b7bfcd849c8486c306ba4e63c7d5476121c
Bertrand Caron <bertrand.fx.caron@gmail.com>
2016-05-10 15:42:50 +1000

    Add some test data

--- test.py
+++ test.py
@@ -4,16 +4,22 @@ def pdb_str(test_ID):
     with open('data/{0}.pdb'.format(test_ID)) as fh:
         return fh.read()
 
+TEST_PAIRS = (
+    (1, 2),
+    (3, 4),
+)
+
 if __name__ == '__main__':
-    pdb_data = [pdb_str(test_ID) for test_ID in (3, 4)]
-    aligned_pdb, alignment_score = align_pdb_on_pdb(
-        reference_pdb_str=pdb_data[0],
-        other_pdb_str=pdb_data[1],
-        silent=False,
-        soft_fail=False,
-    )
+    for test_pair in TEST_PAIRS:
+        pdb_data = [pdb_str(test_ID) for test_ID in test_pair]
+        aligned_pdb, alignment_score = align_pdb_on_pdb(
+            reference_pdb_str=pdb_data[0],
+            other_pdb_str=pdb_data[1],
+            silent=False,
+            soft_fail=False,
+        )
 
-    with open('data/2_on_1.pdb', 'w') as fh:
-        fh.write(aligned_pdb)
+        with open('data/2_on_1.pdb', 'w') as fh:
+            fh.write(aligned_pdb)
 
-    print alignment_score
+        print alignment_score
